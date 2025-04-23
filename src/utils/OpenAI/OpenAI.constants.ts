4c65ed8136b67bcd2ade7980a35451c5fa0c587b
joo-chang <jooc0311@gmail.com>
2025-04-24 00:15:47 +0900

    새로운 모델 추가

--- src/utils/OpenAI/OpenAI.constants.ts
+++ src/utils/OpenAI/OpenAI.constants.ts
@@ -1,6 +1,11 @@
 import { OpenAIModel } from "./OpenAI.types";
 
 export const OpenAIChatModels: Record<string, OpenAIModel> = {
+  "gpt-4.1-mini": {
+    id: "gpt-4.1-mini",
+    name: "GPT-4.1-mini",
+    maxLimit: 32768,
+  },
   "gpt-4o": {
     id: "gpt-4o",
     name: "GPT-4 Omni",
@@ -12,5 +17,15 @@ export const OpenAIChatModels: Record<string, OpenAIModel> = {
   "o3-mini": {
     id: "o3-mini",
     name: "o3-mini",
+  },
+  "gpt-4.1": {
+    id: "gpt-4.1",
+    name: "GPT-4.1",
+    maxLimit: 32768,
+  },
+  "gpt-4o-mini": {
+    id: "gpt-4o-mini",
+    name: "GPT-4o-mini",
+    maxLimit: 100000,
   },
 };
